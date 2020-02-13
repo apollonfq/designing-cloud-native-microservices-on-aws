1b58f00317df57dc551c7e19e55516000e4cfe5c
EC2 Default User <ec2-user@ip-172-31-47-178.ec2.internal>
2020-02-13 16:55:41 +0000

    4th commit

--- deployment/coffeeshop-cdk/lib/coffee-shop-code-pipeline.ts
+++ deployment/coffeeshop-cdk/lib/coffee-shop-code-pipeline.ts
@@ -66,8 +66,8 @@ export class CoffeeShopCodePipeline extends cdk.Stack {
         this.ecrRepository.addLifecycleRule({maxImageAge: cdk.Duration.days(30)});
 
         const defaultSource = codebuild.Source.gitHub({
-            owner: 'humank',
-            repo: 'EventStormingWorkShop',
+            owner: 'apollonfq',
+            repo: 'designing-cloud-native-microservices-on-aws',
             webhook: true, // optional, default: true if `webhookFilteres` were provided, false otherwise
             webhookFilters: [
                 codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH).andBranchIs('master'),
